711b5348ec641386af799270426d5dbc0627f787
THE FABI7 <fabianguancha13@gmail.com>
2024-02-28 22:53:35 -0500

    implementacion en la mainactivity de los layouts

--- app/src/main/java/com/example/colors/MainActivity.java
+++ app/src/main/java/com/example/colors/MainActivity.java
@@ -3,8 +3,32 @@ package com.example.colors;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView lblTargetColor = null;
+
+    private TextView lblProposedColor = null;
+
+    private SeekBar sbrRed = null;
+
+    private TextView lblRedValue = null;
+
+    private SeekBar sbrGreen = null;
+
+    private TextView lblGreenValue = null;
+
+    private SeekBar sbrBlue = null;
+    private TextView lblBlueValue = null;
+    private Button btnNew = null;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
